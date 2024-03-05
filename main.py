a97230d73b15b81e381b535780c2c2af5bc059b8
Guilherme Loschiavo Alvarez <61693858+AlvarezGui@users.noreply.github.com>
2024-03-05 09:41:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def generate_password(leng):
     while i != leng:
         char_or_num = random.randrange(1, 4)
         if char_or_num == 1:
-            password.append(str(random.randrange(1, 10)))
+            password.append(str(random.randrange(0, 10)))
         elif char_or_num == 2:
             password.append(random.choice(string.ascii_letters))
         else:
