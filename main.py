1dc09c49ae392457bd9ca3321701255420812640
Guilherme Loschiavo Alvarez <61693858+AlvarezGui@users.noreply.github.com>
2024-03-05 09:06:45 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+def generate_password(leng):
+    i = 0
+    password = []
+    while i != leng:
+        char_or_num = random.randrange(1, 4)
+        if char_or_num == 1:
+            password.append(str(random.randrange(1, 10)))
+        elif char_or_num == 2:
+            password.append(random.choice(string.ascii_letters))
+        else:
+            password.append(random.choice(string.punctuation))
+        i += 1
+
+    password = ''. join(password)
+    return password
+leng_input = int(input("Leng: "))
+print(generate_password(leng_input))
